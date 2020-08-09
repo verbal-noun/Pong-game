bfacae11da9343c82946da37eeebbeeace3c2496
verbal-noun <kaifahsan420@gmail.com>
2020-08-09 22:25:59 +1000

    Adding player controller script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    float speed = 10f;
+
+    Rigidbody2D rigidBody;
+
+    // Instantiating objects 
+    private void Awake()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(moveUp))
+        {
+           
+            rigidBody.velocity = new Vector2(0.0f, speed);
+        }
+        else if(Input.GetKey(moveDown))
+        {
+            rigidBody.velocity = new Vector2(0.0f, -speed);
+        } 
+        else
+        {
+            rigidBody.velocity = new Vector2(0.0f, 0.0f);
+        }
+    }
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+
+    public float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(11);
+       
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
