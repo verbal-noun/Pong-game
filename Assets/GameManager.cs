3f908182ca99f70678c0f816738aa4bf494699c3
verbal-noun <kaifahsan420@gmail.com>
2020-08-13 23:20:53 +1000

    Adding basic player score tracking

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static int playerScore1;
+    static int playerScore2; 
+
+    public static void Score(string wallname)
+    {
+        if(wallname == "RightWall")
+        {
+            playerScore1 += 1;
+        }
+        else
+        {
+            playerScore2 += 1;
+        }
+
+        Debug.Log("Player 1:" + playerScore1);
+        Debug.Log("Player 2:" + playerScore2);
+    }
+}
---
+++ Assets/SideWalls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+        
+        if(hitInfo.name == "Ball")
+        {
+            GameManager.Score(transform.name);
+        }
+    }
+}
